4aa243b6b8821538e161277b4bc63b7df755d92f
nguyenthanhan201 <fxannguyen201@gmail.com>
2023-01-15 10:18:05 +0700

    fix: fix deploy

--- client/src/components/index/catalog/components/ProductDetail.tsx
+++ client/src/components/index/catalog/components/ProductDetail.tsx
@@ -1,10 +1,11 @@
 import ProductView from "@/components/shared/ProductView/ProductView";
 import Section, { SectionBody } from "@/components/shared/Section";
+import { Product } from "@/lib/redux/slices/products";
 import { useRouter } from "next/router";
 
 const ProductDetailPage = () => {
   const router = useRouter();
-  const product = router.query;
+  const product: any = router.query;
 
   return (
     <Section>
