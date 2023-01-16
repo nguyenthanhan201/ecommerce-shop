faac2375d1921ba27f7b8fe29d2a600ca602f6de
nguyenthanhan201 <fxannguyen201@gmail.com>
2023-01-16 10:40:34 +0700

    ci: optimizer

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NEXT_PUBLIC_BE === "https://ecommerce-shop-be.vercel.app"
-      ? { cssnano: {} }
-      : {}),
   },
 };
